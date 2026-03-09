b4cb438409d241e8f3000db373fb8c1ce6e6c940
vaibhavsinghin7-bit <vaibhavsingh.in7@gmail.com>
2026-03-09 15:57:49 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once("ready", () => {
+    console.log(`Bot is online! Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", message => {
+    if (message.author.bot) return;
+
+    if (message.content.toLowerCase() === "!ping") {
+        message.channel.send("Pong!");
+    }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
